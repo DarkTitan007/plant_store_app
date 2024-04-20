d2908620886fddda3d463e3ce65a85fbdf071e9a
Abhishek J Nair <95077955+DarkTitan007@users.noreply.github.com>
2024-04-20 16:26:55 +0530

    new changes

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_project/Screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+
+    
+    return MaterialApp(
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen());
+  }
+}
